24dfc8677bd7f617aa124d93c6f855ecd80d1cb7
Filipe Corrêa <filipecorrea@br.ibm.com>
2015-11-25 17:07:08 -0200

    Natural language classifier setup

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+//Config Express
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 // Serve the angular client
 app.use('/', express.static(__dirname + '/client'));
 
 // Features
-// require('./server/authentication/authentication.routes.js')(app);
-// require('./server/quotes/quotes.routes.js')(app);
+require('./server/watson/nlc/nlc.routes.js')(app);
 
 // Start server
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.VCAP_APP_PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
